de4029d41ce4eb087de337745a9562a9be396b40
JoinCastl <danilsevcenko759@gmail.com>
2024-02-20 18:41:47 +0300

    коммит

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
-def str_to_uppercase(string):
-    return string.upper()
+def capitalize_words(string):
+    """
+    Функция принимает на вход строку и возвращает ее с заглавными первыми буквами каждого слова.
+
+    Args:
+        string (str): Входная строка.
+
+    Returns:
+        str : Строка с заглавными первыми буквами каждого слова.
+    """
+    words = string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
