52e0734dfb712fad8ad2ac045cda24aa762cf236
JoinCastl <159475451+JoinCastl@users.noreply.github.com>
2024-02-20 18:02:49 +0300

    Update str_func.py
    сделал

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def str_to_uppercase(string):
     return string.upper()
+
+def str_to_uppercase(string):
+    """
+    Функция принимает на вход строку и возвращает ее со всеми заглавными буквами.
+    
+    Args:
+        string (str): Входная строка.
+        
+    Returns:
+        str: Строка с заглавными буквами.
+    """
+    return string.upper()
